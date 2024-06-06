24f6289255c86c83e9356c13565e7d35e29d9e96
Rajkumar <157700974+Rajkumar3754@users.noreply.github.com>
2024-06-06 15:34:40 +0530

    Delete express-backended-contact api directory

--- express-backended-contact api/routes/contactroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getContact, postContact, getallContact, putContact, deleteContact } = require('../controller/contactController');
-
-router.route('/').get(getContact).post(postContact);
-router.route('/:id').get(getallContact).put(putContact).delete(deleteContact);
-
-module.exports = router;
